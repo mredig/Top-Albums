64ec782c56942bae0177c7ad30325b063c543a7a
Michael Redig <mredig@gmail.com>
2020-05-06 17:10:20 -0500

    initial commit

---
+++ Top Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Albums
+//
+//  Created by Michael Redig on 5/6/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
