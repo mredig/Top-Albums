33c133cfeb83b07ad9c4759bd21335fd94740adc
Michael Redig <mredig@gmail.com>
2020-05-07 21:13:24 -0500

    (feat) implemented a lot of the detail vc

--- Top Albums/Result Detail/ResultDetailViewController.swift
+++ Top Albums/Result Detail/ResultDetailViewController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class ResultDetailViewController: UIViewController {
 
+	// MARK: - Properties
 	let musicResult: MusicResult
 	let mainCoordinator: MainCoordinator
 
+	private static let dateFormatter: DateFormatter = {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateStyle = .medium
+		return dateFormatter
+	}()
+
+	// MARK: - Subviews
+	private let albumImageView = UIImageView()
+	private let titleLabel = UILabel()
+	private let artistLabel = UILabel()
+	private let genreLabel = UILabel()
+	private let releaseDateLabel = UILabel()
+	private let copyrightLabel = UILabel()
+	private let itunesStoreButton = UIButton()
+
+	// MARK: - Lifecycle
 	init(musicResult: MusicResult, mainCoordinator: MainCoordinator) {
 		self.musicResult = musicResult
 		self.mainCoordinator = mainCoordinator
 		super.init(nibName: nil, bundle: nil)
+		configureLayout()
+		configureLabels()
+
+		updateViews()
 	}
 
 	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -35,8 +56,80 @@ class ResultDetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-		view.backgroundColor = .white
+		view.backgroundColor = .secondarySystemBackground
     }
 
+	private func configureLayout() {
+		view.addSubview(albumImageView)
+		albumImageView.translatesAutoresizingMaskIntoConstraints = false
+
+		let stack = UIStackView(arrangedSubviews: [titleLabel,
+												   artistLabel,
+												   genreLabel,
+												   releaseDateLabel,
+												   UIView(),
+												   itunesStoreButton,
+												   copyrightLabel
+		])
+		stack.axis = .vertical
+		stack.alignment = .fill
+		stack.distribution = .fill
+		stack.spacing = UIStackView.spacingUseSystem
+		view.addSubview(stack)
+		stack.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			albumImageView.topAnchor.constraint(equalTo: view.topAnchor),
+			albumImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			albumImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			albumImageView.heightAnchor.constraint(equalTo: albumImageView.widthAnchor),
+
+			stack.topAnchor.constraint(equalTo: albumImageView.bottomAnchor, constant: 8),
+			stack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+			stack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+			stack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+		])
+	}
+
+	private func configureLabels() {
+		titleLabel.font = UIFont.systemFont(ofSize: 28, weight: .medium)
+		titleLabel.adjustsFontSizeToFitWidth = true
+		titleLabel.minimumScaleFactor = 0.65
+		artistLabel.font = UIFont.systemFont(ofSize: 19, weight: .medium)
+		artistLabel.textColor = .secondaryLabel
+		genreLabel.font = UIFont.systemFont(ofSize: 17, weight: .light)
+		releaseDateLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+		copyrightLabel.font = UIFont.systemFont(ofSize: 10, weight: .medium)
+
+		itunesStoreButton.setTitle("iTunes Store", for: .normal)
+		itunesStoreButton.setImage(UIImage(systemName: "cart"), for: .normal)
+		itunesStoreButton.setTitleColor(.systemBlue, for: .normal)
+	}
+
+	private func updateViews() {
+		albumImageView.image = UIImage(systemName: "music.note")
+
+		titleLabel.text = musicResult.name
+		artistLabel.text = musicResult.artistName ?? "Unknown artist"
+		genreLabel.text = musicResult.genres.map { $0.name }.filter { $0 != "Music" }.joined(separator: ", ")
+		if let releaseDate = musicResult.releaseDate {
+			releaseDateLabel.text = Self.dateFormatter.string(from: releaseDate)
+		} else {
+			releaseDateLabel.text = "Unknown release date"
+		}
+		copyrightLabel.text = musicResult.copyright ?? "Unknown copyright"
+
+		let imageLoader = mainCoordinator.getImageLoader()
+		_ = imageLoader.fetchImage(for: musicResult) { [weak self] result in
+			DispatchQueue.main.async {
+				do {
+					let imageData = try result.get()
+					self?.albumImageView.image = UIImage(data: imageData)
+				} catch {
+					NSLog("Error loading image in detail VC: \(error)")
+				}
+			}
+		}
+	}
 
 }
